5d5bb534dbdbff3646d25fc487638c92dc5eabc7
Christopher Curl <christopher.r.curl@gmail.com>
2023-01-23 17:15:53 -0800

    specified base units in pint registry

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ Set global unit registry for use throughout the software package
 """
 
 from pint import UnitRegistry, set_application_registry
-ureg = UnitRegistry()
+ureg = UnitRegistry(system='mks')
 set_application_registry(ureg)
 Q_ = ureg.Quantity
