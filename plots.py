2ef3a411ff04668ee7ccaf32dca16a160a37f20f
Christopher Curl <christopher.r.curl@gmail.com>
2023-01-23 17:14:59 -0800

    initial commit

---
+++ plots.py
@@ -0,0 +1,15 @@
+"""
+Plots:
+• Generate a plot of top center surface temperature versus time during the heating process
+for these three materials using both the lumped capacitance and finite difference methods.
+• (550 only) Show a 2D plot of the temperature profile across the full plancha at one-half of
+the time to heat up for the material that is slowest to come to operating temperature.
+"""
+
+
+def plot_top_center_temp_vs_time():
+    return None
+
+
+def plot_temp_profile_at_half_time():
+    return None
