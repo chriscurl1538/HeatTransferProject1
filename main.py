d55ce027da0856cf32c885fefc97eaff2a75f324
Christopher Curl <christopher.r.curl@gmail.com>
2023-01-24 18:53:17 -0800

    added remaining lumped capacitance and energy balance calculations

--- main.py
+++ main.py
@@ -46,13 +46,13 @@ def main():
     rho_cast_iron_300 = Q_(7854, ureg.kg / ureg.meters**3)
     cp_cast_iron_300 = Q_(434, ureg.joules / (ureg.kg * ureg.degK))
     k_cast_iron_300 = Q_(60.5, ureg.watts / (ureg.meters * ureg.degK))
-    alpha_cast_iron_300 = Q_(17.7 * 10**6, ureg.meters**2 / ureg.seconds)
+    alpha_cast_iron_300 = Q_(17.7 * 10**-6, ureg.meters**2 / ureg.seconds)
 
     # Material properties (300K) - Aluminum
     rho_al_300 = Q_(2702, ureg.kg / ureg.meters**3)
     cp_al_300 = Q_(903, ureg.joules / (ureg.kg * ureg.degK))
     k_al_300 = Q_(237, ureg.watts / (ureg.meters * ureg.degK))
-    alpha_al_300 = Q_(97.1 * 10**6, ureg.meters**2 / ureg.seconds)
+    alpha_al_300 = Q_(97.1 * 10**-6, ureg.meters**2 / ureg.seconds)
 
     """
     Analysis
@@ -125,7 +125,59 @@ def main():
     plancha for this. 
     """
 
-    # TODO: Tasks 3-6
+    conv_loss_per_length = cf.calc_convection_heat_loss(h_value=h_air, thickness=thickness, width=width, T_plancha=T_s,
+                                                        T_amb=T_amb).to(ureg.watts / ureg.meters)
+
+    print("The heat lost to convection (per length) when it has reached operating temperature is... ")
+    print("    Heat lost to convection, all materials: {}".format(round(conv_loss_per_length, 2)))
+    print("...")
+
+    """
+    Task 4: If left to continue operating with the stated flux, what would the steady-state temperature 
+    of the center of the plancha be for each material?
+    """
+
+    T_ss = cf.calc_steady_state_temp(flux=heat_flux, h_value=h_air, width=width, thickness=thickness, T_amb=T_amb)
+
+    print("The steady state temperature for the center of the plancha for each material will be identical... ")
+    print("    Steady State Temperature, all materials: {}".format(round(T_ss, 2)))
+    print("...")
+
+    """
+    Task 5: Once the plancha reaches a uniform temperature of 250°C at the initial flux, to what value 
+    should the firepower (applied flux) be reduced to achieve a constant cooking temperature 
+    at 250°C? You may assume uniform temperature on the top and sides of the plancha. 
+    """
+
+    steady_state_flux_250 = cf.calc_convection_heat_loss(h_value=h_air, thickness=thickness, width=width, T_amb=T_amb,
+                                                          T_plancha=T_s)
+
+    print("The flux required for steady state temperature at 250 C will be identical for each material... ")
+    print("    Steady State Applied Flux, all materials: {}".format(round(steady_state_flux_250, 2)))
+    print("...")
+
+    """
+    Task 6: If the convective heat transfer coefficient is reduced by half, how is the time to heat up to 
+    250°C changed?
+    """
+
+    delta_t_lumped_al_half_h = cf.calc_lumped_capacitance(T_amb=T_amb, T_i=T_i, h_value=h_air/2, thickness=thickness,
+                                                          width=width, cp=cp_al_300, flux=heat_flux, T_final=T_s)
+    delta_t_lumped_fireclay_half_h = cf.calc_lumped_capacitance(T_amb=T_amb, T_i=T_i, h_value=h_air/2,
+                                                                rho=rho_fireclay_478, thickness=thickness, width=width,
+                                                                cp=cp_fireclay_478, flux=heat_flux, T_final=T_s)
+    delta_t_lumped_cast_iron_half_h = cf.calc_lumped_capacitance(T_amb=T_amb, T_i=T_i, h_value=h_air/2,
+                                                                 rho=rho_cast_iron_300, thickness=thickness,
+                                                                 width=width, cp=cp_cast_iron_300, flux=heat_flux,
+                                                                 T_final=T_s)
+
+    # TODO: Compare with Finite Difference Method
+
+    print("The time to heat to 250 degC for each material with the convective heat transfer coefficient halved is... ")
+    print("    Lumped Capacitance method, Aluminum: {}".format(round(delta_t_lumped_al_half_h, 2)))
+    print("    Lumped Capacitance method, Ceramic: {}".format(round(delta_t_lumped_fireclay_half_h, 2)))
+    print("    Lumped Capacitance method, Cast Iron: {}".format(round(delta_t_lumped_cast_iron_half_h, 2)))
+    print("...")
 
 
 if __name__ == "__main__":
